852b76f438a43827695fcf9e38a18e3214f742b5
mstorll <m.storll@posteo.de>
2023-10-17 03:02:35 +0200

    update

--- build/docker-entrypoint.d/vnc.sh
+++ build/docker-entrypoint.d/vnc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+sed -i '/^command/d' /app/supervisor/conf.d/x11vnc.conf
 if [ -n "$VNC_PASSWORD" ]; then
     echo -n "$VNC_PASSWORD" > /.password1
     x11vnc -storepasswd $(cat /.password1) /.password2 2>&1 | sed 's/^/+ /'
     chmod 400 /.password*
-    sed -i 's/^command[[:blank:]]*=[[:blank:]]*x11vnc.*/& -rfbauth \/.password2/' /app/supervisor/conf.d/x11vnc.conf
     export VNC_PASSWORD=
+    echo 'command = bash -c "sleep 5 && x11vnc -forever -shared -quiet -rfbauth /.password2"' >> /app/supervisor/conf.d/x11vnc.conf
+else
+    echo 'command = bash -c "sleep 5 && x11vnc -forever -shared -quiet"' >> /app/supervisor/conf.d/x11vnc.conf
 fi

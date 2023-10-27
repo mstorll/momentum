1a7f2d19bd5dadf900c3bff74e76acccf51cbf00
marko.storll <marko.storll@hsh-berlin.com>
2023-10-27 18:12:24 +0200

    update

--- build/docker-entrypoint.d/samba.sh
+++ build/docker-entrypoint.d/samba.sh
@@ -10,4 +10,9 @@ esac
 if [ -n "$RUN_MOMENTUM" ] ; then
   sed -i '/include.*samba.momentum/s/.*include.*/   include = \/app\/samba\/config\/samba.momentum.conf/g' /app/samba/config/samba.conf
 fi
+
+SMB_SERVER_STRING="$(echo "$SMB_SERVER_STRING" | tr -d '/\\"')"
+sed -i "s/__SMB_WORKGROUP__/$SMB_WORKGROUP/g" /app/samba/config/samba.conf
+sed -i "s/__SMB_NETBIOS_NAME__/$SMB_NETBIOS_NAME/g" /app/samba/config/samba.conf
+sed -i "s/__SMB_SERVER_STRING__/$SMB_SERVER_STRING/g" /app/samba/config/samba.conf
 sed -i '/include.*samba.mediaserver.conf/s/.*include.*/   include = \/app\/samba\/config\/samba.mediaserver.conf/g' /app/samba/config/samba.conf
